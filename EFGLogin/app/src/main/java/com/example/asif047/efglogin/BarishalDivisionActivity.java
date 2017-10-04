9d469f57fdc1e4df6d505cae0235c28c8474b095
Asif047 <asifulhaque047@gmail.com>
2017-10-04 22:23:59 +0600

    commit 2 from local6

---
+++ EFGLogin/app/src/main/java/com/example/asif047/efglogin/BarishalDivisionActivity.java
@@ -0,0 +1,13 @@
+package com.example.asif047.efglogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BarishalDivisionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barishal_division);
+    }
+}
