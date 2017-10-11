0f992f0a5c940c4ce53d653e1e63d51a6aa99e37
Asif047 <asifulhaque047@gmail.com>
2017-10-11 17:05:54 +0600

    local17 updated

---
+++ EFGLogin/app/src/main/java/com/example/asif047/efglogin/Hotel/Hotel.java
@@ -0,0 +1,126 @@
+package com.example.asif047.efglogin.Hotel;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by admin on 10/7/2017.
+ */
+
+public class Hotel {
+
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("district_id")
+    @Expose
+    private Integer districtId;
+    @SerializedName("place_name")
+    @Expose
+    private String placeName;
+    @SerializedName("phone")
+    @Expose
+    private String phone;
+    @SerializedName("address")
+    @Expose
+    private String address;
+    @SerializedName("image")
+    @Expose
+    private String image;
+    @SerializedName("details")
+    @Expose
+    private String details;
+    @SerializedName("type")
+    @Expose
+    private String type;
+    @SerializedName("created_at")
+    @Expose
+    private String createdAt;
+    @SerializedName("updated_at")
+    @Expose
+    private String updatedAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getDistrictId() {
+        return districtId;
+    }
+
+    public void setDistrictId(Integer districtId) {
+        this.districtId = districtId;
+    }
+
+    public String getPlaceName() {
+        return placeName;
+    }
+
+    public void setPlaceName(String placeName) {
+        this.placeName = placeName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+
+
+}
